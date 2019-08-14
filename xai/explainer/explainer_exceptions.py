3f7f19a324d7ad184e831225054e5ef4d2bea209
i330688 <sean.saito@sap.com>
2019-08-14 20:02:23 +0800

    Added exceptions

---
+++ xai/explainer/explainer_exceptions.py
@@ -0,0 +1,11 @@
+class DomainNotSupported(Exception):
+    def __init__(self, domain):
+        message = 'Domain {} is currently not supported.'.format(domain)
+        Exception.__init__(self, message)
+        self.message = message
+
+class AlgorithmNotFoundInDomain(Exception):
+    def __init__(self, domain, algorithm):
+        message = 'Algorithm {} is not found in domain {}.'.format(algorithm, domain)
+        Exception.__init__(self, message)
+        self.message = message
