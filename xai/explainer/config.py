ec329af4fff71f11681bccba2cc851ea3522f680
i330688 <sean.saito@sap.com>
2019-08-14 22:49:18 +0800

    Added LimeTabularExplainer as default

--- xai/explainer/config.py
+++ xai/explainer/config.py
@@ -1,9 +1,11 @@
+from xai.explainer.lime_tabular_explainer import LimeTabularExplainer
+
 DEFAULT_ALGORITHM = 'lime'
 DICT_DOMAIN_TO_CLASS = {
     'text': {
         DEFAULT_ALGORITHM: ''
     },
     'tabular': {
-        DEFAULT_ALGORITHM: ''
+        DEFAULT_ALGORITHM: LimeTabularExplainer
     }
 }
