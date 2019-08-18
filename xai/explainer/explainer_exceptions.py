18402fd8271a8a209f49018cdb6c82ccdc1c4b7e
i330688 <sean.saito@sap.com>
2019-08-18 14:39:45 +0900

    Added unsupported mode exception

--- xai/explainer/explainer_exceptions.py
+++ xai/explainer/explainer_exceptions.py
@@ -16,3 +16,8 @@ class ExplainerUninitializedError(Exception):
     def __init__(self, message):
         Exception.__init__(self, message)
         self.message = message
+
+class UnsupportedModeError(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
+        self.message = message
