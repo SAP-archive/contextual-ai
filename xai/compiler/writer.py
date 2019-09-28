9d0be82eb87bad0a52e6cd16138eaf9ef705badd
Chai WeiTah <wei.tah.chai@sap.com>
2019-09-28 15:09:02 +0800

    render component dynamically

---
+++ xai/compiler/writer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 SAP SE or an SAP affiliate company. All rights reserved
+# ============================================================================
+""" Compiler - Components """
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from xai.compiler.base import Dict2Obj
+from xai.formatter import PdfWriter, HtmlWriter
+
+
+################################################################################
+### PDF
+################################################################################
+class Pdf(Dict2Obj):
+
+    def exec(self):
+        return PdfWriter(name=self.name, path=self.path)
+
+################################################################################
+### HTML
+################################################################################
+class Html(Dict2Obj):
+
+    def exec(self):
+        return HtmlWriter(name=self.name, path=self.path)
