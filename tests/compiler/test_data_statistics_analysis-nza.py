5ecd355d9091df029a40941d18090676ef8f38fa
Chai WeiTah <wei.tah.chai@sap.com>
2019-10-06 12:39:13 +0800

    added test case with sample config to debug/fix the issue encountered in complier

--- tests/compiler/test_data_statistics_analysis-nza.py
+++ tests/compiler/test_data_statistics_analysis-nza.py
@@ -3,7 +3,7 @@
 
 # Copyright 2019 SAP SE or an SAP affiliate company. All rights reserved
 # ============================================================================
-"""  Data Statistics Analysis Generator """
+"""  Data Statistics Analysis Generator NZa """
 
 import sys
 sys.path.append('../')
@@ -12,18 +12,13 @@ import unittest
 from xai.compiler.base import Configuration, Controller
 
 
-class TestDataStatisticsAnalysis(unittest.TestCase):
+class TestDataStatisticsAnalysisNZa(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.json = 'sample_template/data-statistics-analysis.json'
-        self.yaml = 'sample_template/data-statistics-analysis.yml'
+        self.json = 'sample_template/data-statistics-analysis_nza.json'
 
     def test_json_generate_report(self):
         controller = Controller(config=Configuration(self.json))
-        controller.render()
-
-    def test_yaml_generate_report(self):
-        controller = Controller(config=Configuration(self.yaml))
         controller.render()
 
 if __name__ == '__main__':
